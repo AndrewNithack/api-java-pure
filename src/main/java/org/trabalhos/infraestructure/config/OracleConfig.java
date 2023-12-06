3db0f7fbb4c220a9fa1948e4ac54d65d4a9165d7
AndrewNithack <douglasnithack@gmail.com>
2023-12-06 19:31:45 -0300

    alteração para conecxao ao banco de dados

--- src/main/java/org/trabalhos/infraestructure/config/OracleConfig.java
+++ src/main/java/org/trabalhos/infraestructure/config/OracleConfig.java
@@ -5,16 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class OracleConfig {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:ORCLCDB";
-    private static final String USERNAME = "system";
-    private static final String PASSWORD = "Oracle_123";
+    private static final String ORACLE_DB_USER = "ADMIN";
+    private static final String ORACLE_DB_PASSWORD = "p@CotucaProjeto2023";
+    private static final String ORACLE_CONN_STR = "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-vinhedo-1.oraclecloud.com))(connect_data=(service_name=g3c8dc0f6a7cb65_aeropecas_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))";
+
+    private static final String URL = "jdbc:oracle:thin:@" + ORACLE_CONN_STR;
+    private static final String USERNAME = ORACLE_DB_USER;
+    private static final String PASSWORD = ORACLE_DB_PASSWORD;
 
     static {
         try {
-            // This explicitly loads the JDBC driver
             Class.forName("oracle.jdbc.driver.OracleDriver");
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Erro ao carregar o driver Oracle JDBC", e);
         }
     }
 
